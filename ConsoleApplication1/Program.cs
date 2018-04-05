24fa518a75f8cafec8f6b306bf7a3a203ab6c663
jkhuang <517070826@qq.com>
2018-04-05 09:18:22 +0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int[] a = new int[100];
+        static void Main(string[] args)
+        {
+       
+            for (int i = 0; i < 100; i++)
+            {
+                a[i] = fn(i);
+                Console.WriteLine(a[i]);
+            }
+
+      
+        }
+
+        static int fn(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return a[fn(n - 1)] + a[fn(n - 2)];
+        }
+
+    }
+}
